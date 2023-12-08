3272b80bd6c7c40fd90aaa8b61d26fc71a34b618
VRKotthapalli <vkotthap@uncc.edu>
2023-12-08 16:57:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ mongoose.connect(process.env.DB_URL,{
     console.log(err);
     console.log("Error connecting to db");
 });
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   };
+  
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/user',require("./routes/loginroute.js"));
 app.use('/budget',authenticate,require("./routes/budgetroute.js"));
